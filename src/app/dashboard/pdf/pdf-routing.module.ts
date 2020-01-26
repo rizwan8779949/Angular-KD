36bb9ad6f34c0a5f99cbb44ba0638ebc2c9ff159
Rizwan Ali <rizwan.ali@novuse.com,rizwan8779949@gmail.com>
2020-01-26 14:02:23 +0530

    some changes

--- src/app/dashboard/pdf/pdf-routing.module.ts
+++ src/app/dashboard/pdf/pdf-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PdfListComponent } from './pdf-list/pdf-list.component';
 import { PdfViewPageComponent } from './pdf-view-page/pdf-view-page.component';
+import { PdfAddComponent } from './pdf-add/pdf-add.component';
 
 const routes: Routes = [
             {
@@ -17,6 +18,11 @@ const routes: Routes = [
                 path: "pdf-view",
                 component:PdfViewPageComponent
             }, 
+            {
+                path: "pdf-add",
+                component:PdfAddComponent
+            }, 
+            
 ];   
 
 @NgModule({
